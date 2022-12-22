31e77870356e619d359eda57ee6b1b27aa555999
relixilon <relixilon@gmail.com>
2022-12-22 19:31:30 +0000

    working with docker

--- vue.config.js
+++ vue.config.js
@@ -1,10 +1,5 @@
 const { defineConfig } = require('@vue/cli-service')
 
-module.exports = {
-  publicPath: process.env.NODE_ENV === 'production'
-    ? '/Bar-Frontend/'
-    : '/'
-}
 
 module.exports = defineConfig({
   transpileDependencies: true,
