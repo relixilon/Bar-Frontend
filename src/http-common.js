6f9139cd42ba79349e32c8dab4edbce61fb0c4cc
Mario <45695964+relixilon@users.noreply.github.com>
2023-01-03 13:23:19 +0100

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -2,7 +2,7 @@ import axios from "axios";
 axios.defaults.withCredentials = true;
 
 export default axios.create({
-  baseURL: "http://192.168.1.13:3000/api",
+  baseURL: "http://3.69.70.35:3000/api",
   headers: {
     crossDomain: true,
     'Access-Control-Allow-Credentials': true,
